11bc55d536b105cc32db7c4d2594e5af3437e64b
jelassiahmed <47889856+jelassiahmed@users.noreply.github.com>
2021-03-30 15:52:44 +0100

    Update GerantController.php
    Security fixes

--- src/Controller/GerantController.php
+++ src/Controller/GerantController.php
@@ -12,8 +12,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Dompdf\Dompdf;
 use Dompdf\Options;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
+ * @IsGranted("ROLE_ADMIN")
  * @Route("/admin/gerant")
  */
 class GerantController extends AbstractController
